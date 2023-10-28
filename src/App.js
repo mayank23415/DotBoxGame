336a0f4c48c98928a81d336dbbd5f17a104c356c
mayank23415 <52768803+mayank23415@users.noreply.github.com>
2023-10-28 10:01:04 +0530

    Splash Screen Addition

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect} from 'react';
 import GameContainer from './containers/GameContainer';
 import './App.global.css';
+import { Splash } from './splash-screens/Splash';
 
-class App extends Component {
-	render() {
-		return (
-			<div className="app">
-				<GameContainer />
-			</div>
-		);
-	}
-}
+const App = () => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000); 
+	}, []);
+
+	return (
+		<div className='app'>
+			{loading ? <Splash /> : <GameContainer />}
+		</div>
+	);
+};
 
 export default App;
---
+++ src/splash-screens/Splash.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles/Splash.style.css';
+
+export function Splash() {
+    return (
+        <div className="splash-screen">
+            <p>La Pipopipette</p>
+        </div>
+    )
+}
